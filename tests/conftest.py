6bbcf5f4919fa0fc93b74cf6aa8924bccb377d18
Richard Chien <stdrc@outlook.com>
2025-10-21 02:20:45 +0800

    fix ut
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,9 +38,7 @@ from kimi_cli.tools.web.search import SearchWeb
 @pytest.fixture
 def config() -> Config:
     """Create a Config instance."""
-    default_config = get_default_config()
-    default_config.services.moonshot_search = MoonshotSearchConfig(api_key=SecretStr("sk-abc"))
-    return default_config
+    return get_default_config()
 
 
 @pytest.fixture
