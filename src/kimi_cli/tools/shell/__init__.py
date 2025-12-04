f02f7b02a52bdf81117bd9061b9c9b788d502c43
Richard Chien <stdrc@outlook.com>
2025-12-04 23:03:04 +0800

    fix
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/kimi_cli/tools/shell/__init__.py
+++ src/kimi_cli/tools/shell/__init__.py
@@ -109,6 +109,6 @@ async def _run_shell_command(
 
 def _shell_args(command: str) -> tuple[str, ...]:
     if platform.system() == "Windows":
-        return ("cmd.exe", "/c", command)
+        return ("cmd.exe", "/d", "/s", "/c", command)
 
     return ("/bin/sh", "-c", command)
