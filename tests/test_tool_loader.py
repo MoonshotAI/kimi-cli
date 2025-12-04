3a3e7617526989c9db4e1e665635513c25ea2555
leslie <ly624928214@gmail.com>
2025-12-04 18:55:48 +0800

    feat: add injector-based tool loader with clearer errors

--- tests/test_tool_loader.py
+++ tests/test_tool_loader.py
@@ -9,9 +9,13 @@ from kimi_cli.soul.agent import _load_tool
 from kimi_cli.soul.injector import Injector, ToolDependencyError, ToolLoadError
 
 
-def _install_fake_module(name: str, cls_name: str, cls_obj: type) -> None:
+def _install_fake_module(
+    name: str, cls_name: str, cls_obj: type, *, extra_classes: list[type] | None = None
+) -> None:
     module = types.ModuleType(name)
     setattr(module, cls_name, cls_obj)
+    for cls in extra_classes or []:
+        setattr(module, cls.__name__, cls)
     sys.modules[name] = module
 
 
@@ -25,7 +29,7 @@ def test_load_tool_success_injects_dependency():
 
     module_name = "tests.fake_tool_module_success"
     cls_name = "GoodTool"
-    _install_fake_module(module_name, cls_name, GoodTool)
+    _install_fake_module(module_name, cls_name, GoodTool, extra_classes=[Dep])
 
     dep_instance = Dep()
     injector = Injector({Dep: dep_instance})
@@ -48,7 +52,7 @@ def test_load_tool_missing_dependency_raises():
 
     module_name = "tests.fake_tool_module_missing_dep"
     cls_name = "NeedsDep"
-    _install_fake_module(module_name, cls_name, NeedsDep)
+    _install_fake_module(module_name, cls_name, NeedsDep, extra_classes=[Dep])
 
     injector = Injector({})
 
