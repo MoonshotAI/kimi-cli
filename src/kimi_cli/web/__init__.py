92090b854b0884dae2fb767e9a80318c81abe343
kevinWangSheng <ltwoggbuty2mh@gmail.com>
2026-04-20 05:24:38 -0700

    fix(web): guard web imports from cwd module shadowing

--- src/kimi_cli/web/__init__.py
+++ src/kimi_cli/web/__init__.py
@@ -1,5 +1,12 @@
 """Kimi Code CLI Web Interface."""
 
-from kimi_cli.web.app import create_app, run_web_server
+import importlib
+
+from . import _safe_imports as _safe_imports
+
+_ = _safe_imports
+_app = importlib.import_module("kimi_cli.web.app")
+create_app = _app.create_app
+run_web_server = _app.run_web_server
 
 __all__ = ["create_app", "run_web_server"]
